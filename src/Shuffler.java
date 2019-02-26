14bab18b05991001a578256259a012d6440bd4dc
BT_1N3_22 <rzhao3818@bths.edu>
2019-02-26 14:42:30 -0500

    almost

--- src/Shuffler.java
+++ src/Shuffler.java
@@ -50,8 +50,19 @@ public class Shuffler {
      * @param values is an array of integers simulating cards to be shuffled.
      */
     public static void perfectShuffle(int[] values) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 3 *** */
+        int[] shuffled = new int[values.length];
+        int k = 0;
+        for (int j = 0; j < (values.length+ 1)/2; j ++){
+            values[j] = shuffled[k];
+            k += k+2;
+        }
+        k=1;
+        for (int j =(values.length+1)/2; j < values.length; j++){
+            values [j] = shuffled[k];
+            k += k+2;
+        }
     }
+
 
     /**
      * Apply an "efficient selection shuffle" to the argument.
